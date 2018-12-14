8ec67bd7b1b063c5ad4654463597b266f36d634b
momo <1445107381@qq.com>
2018-12-15 01:20:28 +0800

    update js

--- main.js
+++ main.js
@@ -1,31 +1,20 @@
 $(function(){
-    // let x = true
-    // $('.button').click( function(e){
-    //    if(x){
-    //     $('.float').addClass('active')
+    let x = true
+    $('.button').click( function(e){
+       if(x){
+        $('.float').addClass('active')
         
-    //    }else{
-    //     $('.float').removeClass('active')
-    //    }
-    //    x=!x
-    //   })
-    $('.button').on('click', () => {
-        let flag= true
-        if (flag) {
-            $('.float').addClass('active')
-            flag = false
-        } else {
-            $('.float').removeClass('active')
-            flag = true
-        }
-    })
-
+       }else{
+        $('.float').removeClass('active')
+       }
+       x=!x
+      })
       $('.wrap').on('click',function(e){
         e.stopPropagation()
        })
 
       $(document).on('click',function(){
         $('.float').removeClass('active')
-        flag=true
+        x=true
       })
 })
